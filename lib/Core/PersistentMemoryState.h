edfc828aeffecd322d0cbb46e6d18b70d2143c74
bgreeves <bgreeves@umich.edu>
2020-01-29 15:12:35 -0500

    Reorganize PMState header and make nice comment

--- lib/Core/PersistentMemoryState.h
+++ lib/Core/PersistentMemoryState.h
@@ -10,7 +10,58 @@
 
 namespace klee {
 
+/// Software that uses non-volatile main memory needs to properly ensure
+/// that updates to memory get persisted.
+///
+/// Once a region of persistent memory has been modified, that write will be
+/// flushed from cache to memory at some point in the future. The range of
+/// time during which the write could persist is called the "persistence interval."
+///
+/// Persistence intervals for data are essentially indefinite unless the program
+/// explicitly instructs the cache to be written back. After issuing a flush/clwb
+/// instruction, the interval is _still_ indefinite, because processors may delay
+/// execution of the flush instructions. After issuing a flush AND a memory fence,
+/// the modification is guaranteed to be persisted.
+///
+/// The entire execution of a program can be broken up into "persistence epochs"
+/// delimited by sfence instructions. Any memory operation (store, flush/clwb)
+/// is issued and completes during one persistence epoch.
+///
+/// This class tracks modifications, flushes, and fences for persistent memory.
+/// At any point in time, a region of memory can be queried to determine if it
+/// is definitely persisted. You can also query pairs of regions to check if
+/// modifications to one region are guaranteed to have persisted before
+/// modifications to the other.
 class PersistentMemoryState {
+public:
+  PersistentMemoryState();
+
+  /// Mark the persistent range between [base, base+size) as modified.
+  void store(uint64_t base, uint64_t size);
+
+  /// Notify the state of a flush instruction to the cache line containing addr.
+  void flush(uint64_t addr);
+
+  /// Notify the state of an sfence instruction.
+  void fence();
+
+  /// Return true if the most recent store(s) to the persistent range
+  /// [base, base+size) is/are guaranteed to be persisted at this time.
+  bool isPersisted(uint64_t base, uint64_t size) const;
+
+  /// Return true if the most recent store(s) to range [baseA, baseA+sizeA)
+  /// is/are guaranteed to be persisted before any of the most recent stores to
+  /// range [baseB, baseB+sizeB).
+  bool isOrderedBefore(uint64_t baseA, uint64_t sizeA,
+                       uint64_t baseB, uint64_t sizeB) const;
+
+  void print(llvm::raw_ostream &os) const;
+
+private:
+  /// Current persistence epoch.
+  /// Epochs are delimited by memory fence instructions.
+  unsigned currEpoch;
+
 public:
   typedef boost::icl::interval<uint64_t>::type addr_range;
 
@@ -37,19 +88,12 @@ public:
   };
 
 private:
-  /// Current persistence epoch.
-  /// Epochs are delimited by memory fence instructions.
-  /// The first epoch is 0.
-  unsigned currEpoch;
-
-  /// Maps persistent memory ranges [start_addr, end_addr) to a pair
-  /// of ints representing that memory range's persist interval.
-  /// The persist interval begins at the epoch of the most recent
-  /// modification to the range and ends at the epoch where it
-  /// is fully persisted.
+  /// For each persistent memory range [start_addr, end_addr), store
+  /// a pair of ints representing that range's persistence interval.
   boost::icl::interval_map<uint64_t, PersistInterval> persistIntervals;
 
   /// For each cache line, keep track of when it was last flushed.
+  /// Flushes are not committed into this map until the epoch ends.
   boost::icl::interval_map<uint64_t, unsigned> lastFlushedEpoch;
 
   /// Holds all memory ranges that have unresolved persist intervals
@@ -64,32 +108,6 @@ private:
   /// to them during the current epoch.
   boost::icl::split_interval_set<uint64_t> flushedThisEpoch;
 
-public:
-  PersistentMemoryState();
-
-  /// Mark the persistent range between [base, base+size) as modified.
-  void store(uint64_t base, uint64_t size);
-
-  /// Mark the cache line containing addr as flushed.
-  void flush(uint64_t addr);
-
-  /// Notify the state that the persistence epoch is finishing.
-  void fence();
-
-  /// Return true if the most recent store(s) to the persistent range
-  /// [base, base+size) is/are guaranteed to be persisted at this time.
-  bool isPersisted(uint64_t base, uint64_t size) const;
-
-  /// Return true if the persistence interval for the most recent
-  /// store to range [baseA, baseA+sizeA) has no overlap with the
-  /// persistence interval for the most recent store to
-  /// range [baseB, baseB+sizeB).
-  bool isOrderedBefore(uint64_t baseA, uint64_t sizeA,
-                       uint64_t baseB, uint64_t sizeB) const;
-
-  void print(llvm::raw_ostream &os) const;
-
-private:
   bool isFullyFlushed(const addr_range &range) const;
 };
 
