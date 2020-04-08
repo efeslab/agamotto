59cf68c95910bf93272115ccf867da8523e6fb4f
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-04-08 15:49:43 -0400

    Fixed mmap abort, but seems to be some permission issue

--- nvmbugs/004_heuristic_correctness/nvm_or_vol.c
+++ nvmbugs/004_heuristic_correctness/nvm_or_vol.c
@@ -1,22 +1,7 @@
 #include "common.h"
 
 /**
- * Without heuristic: 3 paths (2 regular, 1 after error) 
- * -- 54 inst
- * With heurisitc: 2 paths, 1 terminated early
- * -- 47 inst
  */
-
-int mod_function(char *addr, bool mod) {
-    char a = 2;
-    if (mod) {
-        addr[0] = a;
-    } else {
-        a = addr[0];
-    }
-
-    return a;
-}
 
 int main(int argc, char *argv[]) {
     char pmemaddr[BUF_LEN];
