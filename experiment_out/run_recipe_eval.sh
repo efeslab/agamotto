3a6b416a737878d684f1dbd0397bb0ecc26eb6f7
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-05-15 20:47:12 -0400

    Commit results

--- experiment_out/run_recipe_eval.sh
+++ experiment_out/run_recipe_eval.sh
@@ -8,32 +8,32 @@ OUTDIR=../../experiment_out
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread.bc --sym-pmem-init-from pool --sym-arg 1 &
+  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
 
 # 2. Default (random-path + covnew)
 ./klee --output-dir=$OUTDIR/klee-recipe-nothread-default \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread.bc --sym-pmem-init-from pool --sym-arg 1 &
+  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
 
 # 3. BFS
 ./klee --output-dir=$OUTDIR/klee-recipe-nothread-bfs --search=bfs \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread.bc --sym-pmem-init-from pool --sym-arg 1 &
+  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
 
 # 4. DFS
 ./klee --output-dir=$OUTDIR/klee-recipe-nothread-dfs --search=dfs \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread.bc --sym-pmem-init-from pool --sym-arg 1 &
+  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
 
 # 5. Coverage new 
 ./klee --output-dir=$OUTDIR/klee-recipe-nothread-covnew --search=nurs:covnew \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread.bc --sym-pmem-init-from pool --sym-arg 1 &
+  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
