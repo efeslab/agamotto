755575e5047078da0704522a3fa5431b73a225a1
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-05-17 17:37:10 -0400

    Add recipe stuff, scripts

--- experiment_out/run_recipe_tx_eval.sh
+++ experiment_out/run_recipe_tx_eval.sh
@@ -3,41 +3,41 @@ set +x
 
 OUTDIR=../../experiment_out
 
-cp clht-nothread-sym-friendly.bc ../build/bin
+cp clht-nothread-transaction.bc ../build/bin
 cd ../build/bin
 ulimit -s unlimited
 
 # 1. Agamotto
-./klee --output-dir=$OUTDIR/klee-recipe-notx-static --search=nvm --nvm-heuristic-type=static \
+./klee --output-dir=$OUTDIR/klee-recipe-tx-static --search=nvm --nvm-heuristic-type=static \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
+  clht-nothread-transaction.bc --sym-pmem-init-from pool --sym-arg 2 &
 
 # 2. Default (random-path + covnew)
-./klee --output-dir=$OUTDIR/klee-recipe-notx-default \
+./klee --output-dir=$OUTDIR/klee-recipe-tx-default \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
+  clht-nothread-transaction.bc --sym-pmem-init-from pool --sym-arg 2 &
 
 # 3. BFS
-./klee --output-dir=$OUTDIR/klee-recipe-notx-bfs --search=bfs \
+./klee --output-dir=$OUTDIR/klee-recipe-tx-bfs --search=bfs \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
+  clht-nothread-transaction.bc --sym-pmem-init-from pool --sym-arg 2 &
 
 # 4. DFS
-./klee --output-dir=$OUTDIR/klee-recipe-notx-dfs --search=dfs \
+./klee --output-dir=$OUTDIR/klee-recipe-tx-dfs --search=dfs \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
+  clht-nothread-transaction.bc --sym-pmem-init-from pool --sym-arg 2 &
 
 # 5. Coverage new 
-./klee --output-dir=$OUTDIR/klee-recipe-notx-covnew --search=nurs:covnew \
+./klee --output-dir=$OUTDIR/klee-recipe-tx-covnew --search=nurs:covnew \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
   --disable-verify=true --libc=uclibc --link-known-lib=libpmemobj --link-known-lib=libpmem \
   --libcxx --posix-runtime --env-file=pmdk.env \
-  clht-nothread-sym-friendly.bc --sym-pmem-init-from pool --sym-arg 2 &
+  clht-nothread-transaction.bc --sym-pmem-init-from pool --sym-arg 2 &
