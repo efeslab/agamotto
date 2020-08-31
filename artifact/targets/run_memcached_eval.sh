2cbc79c93dd2ed0eb2ebf9eb53c247a8f7d09c71
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-08-31 14:24:34 -0400

    Update

--- artifact/targets/run_memcached_eval.sh
+++ artifact/targets/run_memcached_eval.sh
@@ -1,45 +1,24 @@
 #! /bin/bash
-set +x
+set -x
 
+source common.sh
 
-OUTDIR=./
+MCD=$DIR/memcached-pmem-prebuilt
 
-# 1. Agamotto
-./klee --output-dir=$OUTDIR/klee-memcached-static --search=nvm --nvm-heuristic-type=static \
+# Agamotto's search strategy
+$BUILD/bin/klee --output-dir=$OUTDIR/klee-memcached-static --search=nvm --nvm-heuristic-type=static \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
-  --disable-verify=true --libc=uclibc --link-known-lib=libevent --link-known-lib=libpmem \
+  --disable-verify=true --libc=uclibc \
+  --link-llvm-lib=$MCD/libevent.so.bc --link-llvm-lib=$MCD/libpmem.so.bc \
   --posix-runtime \
-  --env-file=memcached.env memcached.bc --sym-pmem-delay PMEM 8388608 --sock-handler memcached_rand \
+  --env-file=$MCD/memcached.env $MCD/memcached.bc --sym-pmem-delay PMEM 8388608 --sock-handler memcached_rand \
   -m 0 -U 0 -t 1 -A -o pslab_file=PMEM,pslab_size=8,pslab_force &
 
-# 2. Default (random-path + covnew)
-./klee --output-dir=$OUTDIR/klee-memcached-default \
+# KLEE's default search strategy (random-path + covnew)
+$BUILD/bin/klee --output-dir=$OUTDIR/klee-memcached-default \
   --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
-  --disable-verify=true --libc=uclibc --link-known-lib=libevent --link-known-lib=libpmem \
+  --disable-verify=true --libc=uclibc \
+  --link-llvm-lib=$MCD/libevent.so.bc --link-llvm-lib=$MCD/libpmem.so.bc \
   --posix-runtime \
-  --env-file=memcached.env memcached.bc --sym-pmem-delay PMEM 8388608 --sock-handler memcached_rand \
-  -m 0 -U 0 -t 1 -A -o pslab_file=PMEM,pslab_size=8,pslab_force &
-
-# 3. BFS
-./klee --output-dir=$OUTDIR/klee-memcached-bfs --search=bfs \
-  --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
-  --disable-verify=true --libc=uclibc --link-known-lib=libevent --link-known-lib=libpmem \
-  --posix-runtime \
-  --env-file=memcached.env memcached.bc --sym-pmem-delay PMEM 8388608 --sock-handler memcached_rand \
-  -m 0 -U 0 -t 1 -A -o pslab_file=PMEM,pslab_size=8,pslab_force &
-
-# 4. DFS
-./klee --output-dir=$OUTDIR/klee-memcached-dfs --search=dfs \
-  --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
-  --disable-verify=true --libc=uclibc --link-known-lib=libevent --link-known-lib=libpmem \
-  --posix-runtime \
-  --env-file=memcached.env memcached.bc --sym-pmem-delay PMEM 8388608 --sock-handler memcached_rand \
-  -m 0 -U 0 -t 1 -A -o pslab_file=PMEM,pslab_size=8,pslab_force &
-
-# 5. Coverage new 
-./klee --output-dir=$OUTDIR/klee-memcached-covnew --search=nurs:covnew \
-  --custom-checkers=false --max-time=86400 --write-errors-only=true --max-memory=10000  \
-  --disable-verify=true --libc=uclibc --link-known-lib=libevent --link-known-lib=libpmem \
-  --posix-runtime \
-  --env-file=memcached.env memcached.bc --sym-pmem-delay PMEM 8388608 --sock-handler memcached_rand \
+  --env-file=$MCD/memcached.env $MCD/memcached.bc --sym-pmem-delay PMEM 8388608 --sock-handler memcached_rand \
   -m 0 -U 0 -t 1 -A -o pslab_file=PMEM,pslab_size=8,pslab_force &
