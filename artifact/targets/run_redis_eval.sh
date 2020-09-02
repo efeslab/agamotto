0c83c8a54ff4bc8690ab481ba3e8209f8ebd3e4c
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-09-01 23:58:27 -0400

    Got the other redis up and running again as well.

--- artifact/targets/run_redis_eval.sh
+++ artifact/targets/run_redis_eval.sh
@@ -1,84 +1,17 @@
 #! /bin/bash
 
-# KLEE_NVM_BUILD=../
-# KLEE=$(realpath ./klee)
-
 source common.sh
 
-REDISD=$DIR/redis-pmem-prebuilt
+cd redis-pmem-2-prebuilt
 
-PMEM=1
+# 1. Agamotto
 
-if [[ $PMEM -eq 1 ]]; then
-	PMEM_FILE_NAME=$REDISD/redis.pmem
-	# PMEM_FILE_SIZE=2147483648  # 2 * 1024 * 1024 * 1024
-	# PMEM_FILE_SIZE=67108864  # 64 * 1024 * 1024
-	PMEM_FILE_SIZE=8388608  # 8 * 1024 * 1024
-	PMEM_INIT_OPTS="--sym-pmem-init-from $PMEM_FILE_NAME"
+rm -rf $OUTDIR/klee-redis-static
 
-	REDIS_CONF=$REDISD/redis-pmem.conf
-	SEARCH="--search=nvm --nvm-heuristic-type=static"
-	# SEARCH="--search=nurs:covnew"
-	# SEARCH="--search=nurs:depth"
-	# SEARCH="--search=dfs"
+# ./run.sh build/redis-server.bc "--search=nvm --nvm-heuristic-type=static" "$OUTDIR/klee-redis-static" &
 
-	# REDIS_OUT=$OUTDIR/klee-redis-depth
-	REDIS_OUT=$OUTDIR/klee-redis-static
+# 2. Default (random-path + covnew)
 
-	# SEARCH="$SEARCH --use-batching-search"
-else
-	PMEM_INIT_OPTS=
-	REDIS_CONF=$REDISD/redis.conf
-	SEARCH="--search=nurs:covnew"
-fi
+rm -rf $OUTDIR/klee-redis-default
 
-CLIENT_FILE_NAME=$REDISD/client.txt
-SERVER_PORT=6379
-
-TCP_SYM_SIZE=100
-TCP_OPTS="--tcp-client-file redis-client $SERVER_PORT $CLIENT_FILE_NAME"
-# TCP_OPTS="--tcp-client-sym redis-client $SERVER_PORT $TCP_SYM_SIZE"
-# TCP_OPTS="--sock-handler redis_simple_concrete"
-# TCP_OPTS="--sock-handler redis_symbolic"
-#TCP_OPTS="--sock-handler redis_semi_symbolic"
-
-cat > $REDISD/pmdk.env << EOF
-PMEMOBJ_LOG_LEVEL=0
-PMEM_LOG_LEVEL=0
-PMEM_NO_FLUSH=0
-PMEM_IS_PMEM_FORCE=1
-EOF
-
-# --link-llvm-lib=$KLEE_NVM_BUILD/nvmbugs/pmdk/install/lib/pmdk_debug/libvmmalloc.so.bc \
-	# --search=bfs \
-	# --all-external-warnings \
-	# --log-partial-queries-early \
-	# --search=nvm \
-	# --nvm-heuristic-type=static \
-	# --output-module \
-	# --posix-debug \
-	# --write-paths \
-	# --very-verbose \
-set -x
-
-rm -rfv $REDIS_OUT
-
-$KLEE \
-	$SEARCH \
-	--disable-verify=true \
-	--max-memory=128000 \
-	--max-time=300 \
-	--output-dir=$REDIS_OUT \
-	--libc=uclibc \
-	--link-known-lib=libpmem \
-	--link-known-lib=libpmemobj \
-	--link-llvm-lib=$REDISD/libpmem.so.bc \
-	--link-llvm-lib=$REDISD/libpmemobj.so.bc \
-	--link-llvm-lib=$REDISD/libhiredis.bca \
-	--link-llvm-lib=$REDISD/liblua.bca \
-  	$REDISD/redis-server.bc \
-	--posix-runtime \
-	--env-file=$REDISD/pmdk.env \
-	$PMEM_INIT_OPTS \
-	$TCP_OPTS \
-	$REDIS_CONF
+./run.sh build/redis-server.bc "--search=dfs" "$OUTDIR/klee-redis-default" &
