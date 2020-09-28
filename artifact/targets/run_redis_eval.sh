b12dd0424089a3c74495188cd5876e2a5dae335b
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-09-27 22:40:33 -0400

    Reduce Redis memory consumption

--- artifact/targets/run_redis_eval.sh
+++ artifact/targets/run_redis_eval.sh
@@ -4,14 +4,14 @@ source common.sh
 
 cd redis-pmem-2-prebuilt
 
-# 1. Agamotto
-
-rm -rf $OUTDIR/klee-redis-static
-
-./run.sh build/redis-server.bc "--search=nvm --nvm-heuristic-type=static" "$OUTDIR/klee-redis-static" &
-
-# 2. Klee
+# 1. Klee
 
 rm -rf $OUTDIR/klee-redis-default
 
-./run.sh build/redis-server.bc "--search=dfs" "$OUTDIR/klee-redis-default" &
+./run.sh build/redis-server.bc "--search=dfs" "$OUTDIR/klee-redis-default"
+
+# 2. Agamotto
+
+rm -rf $OUTDIR/klee-redis-static
+
+./run.sh build/redis-server.bc "--search=nvm --nvm-heuristic-type=static" "$OUTDIR/klee-redis-static" 
